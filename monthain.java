c43dfac2f88605ff476f6714cff94999d818277e
Kim Slava <angelshaggy99@gmail.com>
2025-07-06 17:59:05 +0900

    init

---
+++ monthain.java
@@ -0,0 +1,8 @@
+public class monthain {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 29;
+        System.out.println("The month is: " + y);
+        System.out.println("The year is: " + x);
+    }
+}
