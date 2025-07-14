9152dab646a9aebc18dc14c905d47dfeb6390322
Kim Slava <74595312+Disscent21@users.noreply.github.com>
2025-07-14 18:56:23 +0900

    Delete monthain.java

--- monthain.java
+++
@@ -1,8 +0,0 @@
-public class monthain {
-    public static void main(String[] args) {
-        int x = 20;
-        int y = 29;
-        System.out.println("The month is: " + y);
-        System.out.println("The year is: " + x);
-    }
-}
