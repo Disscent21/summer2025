7465461302ba284b1ed2032e175935e63f185931
Kim Slava <74595312+Disscent21@users.noreply.github.com>
2025-07-14 18:46:50 +0900

    Add files via upload

---
+++ SimpleFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class SimpleFrame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Chloe Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        GamePanel gamePanel = new GamePanel();
+        frame.add(gamePanel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
